f0a8aeced87316c8ca551ef288a513eb60469b74
APlatonenkov <platonenkov87@gmail.com>
2021-08-12 11:49:04 +0300

    оптимизация методов

--- CLI.Tag.Helper/CLITagHelper.cs
+++ CLI.Tag.Helper/CLITagHelper.cs
@@ -11,7 +11,7 @@ namespace CLI.Tag.Helper
     {
         #region Arguments value
 
-        /// <summary> Проверка что за тегом следует его аргумент а не другой тег</summary>
+        /// <summary> Проверка что по индексу лежит есть параметр, но не другой тег, и возвращает его значение </summary>
         /// <param name="args">аргументы ком. строки</param>
         /// <param name="index">текущий индекс тега</param>
         /// <param name="ErrorIfNoValue">Обязательно наличие параметра у тега (в случае отсутствия - будет ошибка)</param>
@@ -23,7 +23,7 @@ namespace CLI.Tag.Helper
             return args[index + 1];
         }
 
-        /// <summary> Проверка что за тегом следует его аргумент а не другой тег</summary>
+        /// <summary> Проверка что за тегом следует его аргумент а не другой тег, и возвращает его значение </summary>
         /// <param name="args">аргументы ком. строки</param>
         /// <param name="tag">текущий тег</param>
         /// <param name="tag_index">индекс текущего тега</param>
@@ -93,37 +93,42 @@ namespace CLI.Tag.Helper
         /// Вывод на консоль доступных команд CLI
         /// </summary>
         /// <param name="lang">язык для вывода</param>
+        /// <param name="NeedCloseAfterPrint">Закрыть приложение если будет распечатан help</param>
         /// <param name="TagFilePath">путь к файлу тегов (если Null - используется PROJECT_NAME.Tags.json)</param>
         /// <returns></returns>
-        public static async Task PrintHelpInfoAsync(string lang, string TagFilePath = null)
+        public static async Task PrintHelpInfoAsync(string lang, bool NeedCloseAfterPrint, string TagFilePath = null)
         {
             var help_tags = await GetLocalizedTagsAsync(lang, TagFilePath);
 
-            if (help_tags?.Tags is null)
-            {
-                "The program has no reference information".ConsoleRed();
-                return;
-            }
-
-            foreach (var tag in help_tags.Tags)
-                tag.ConsolePrint();
+            PrintHelp(help_tags, NeedCloseAfterPrint);
         }
         /// <summary> Вывод на консоль доступных команд CLI </summary>
         /// <param name="lang">язык для вывода</param>
+        /// <param name="NeedCloseAfterPrint">Закрыть приложение если будет распечатан help</param>
         /// <param name="TagFilePath">путь к файлу тегов (если Null - используется PROJECT_NAME.Tags.json)</param>
         /// <returns></returns>
-        public static void PrintHelpInfo(string lang, string TagFilePath = null)
+        public static void PrintHelpInfo(string lang, bool NeedCloseAfterPrint, string TagFilePath = null)
         {
             var help_tags = GetLocalizedTags(lang, TagFilePath);
 
+            PrintHelp(help_tags, NeedCloseAfterPrint);
+        }
+
+        private static void PrintHelp(LocalizedTags help_tags, bool NeedCloseAfterPrint)
+        {
             if (help_tags?.Tags is null)
             {
                 "The program has no reference information".ConsoleRed();
+                if (NeedCloseAfterPrint)
+                    Environment.Exit(1);
                 return;
             }
 
             foreach (var tag in help_tags.Tags)
                 tag.ConsolePrint();
+
+            if (NeedCloseAfterPrint)
+                Environment.Exit(1);
         }
 
         /// <summary>
@@ -131,83 +136,78 @@ namespace CLI.Tag.Helper
         /// </summary>
         /// <param name="args">аргументы ком. строки</param>
         /// <param name="NeedCloseAfterPrint">Закрыть приложение если будет распечатан help</param>
+        /// <param name="TagFilePath">путь к файлу тегов (если Null - используется PROJECT_NAME.Tags.json)</param>
         /// <returns></returns>
-        public static async Task CheckForHelpTag_PrintAndCloseAsync(string[] args, bool NeedCloseAfterPrint = true)
+        public static async Task CheckForHelpTag_PrintAndCloseAsync(string[] args, bool NeedCloseAfterPrint = true, string TagFilePath = null)
         {
             if (args.Length <= 0 || !IsItHelpTag(args[0]))
                 return;
 
             var lang = FindLanguageTagValue(args);
-            await PrintSupportedLanguagesAsync();
-            await PrintHelpInfoAsync(lang); //en-Us, en, En - what lang or culture name you need
-            if (NeedCloseAfterPrint)
-                Environment.Exit(1);
+            await PrintSupportedLanguagesAsync(TagFilePath);
+            await PrintHelpInfoAsync(lang, NeedCloseAfterPrint, TagFilePath); //en-Us, en, En - what lang or culture name you need
         }
         /// <summary>
         /// Проверяет необходимость вывода help в консоль (если тег -h или --help является первым тегом)
         /// </summary>
         /// <param name="args">аргументы ком. строки</param>
         /// <param name="NeedCloseAfterPrint">Закрыть приложение если будет распечатан help</param>
+        /// <param name="TagFilePath">путь к файлу тегов (если Null - используется PROJECT_NAME.Tags.json)</param>
         /// <returns></returns>
-        public static void CheckForHelpTag_PrintAndClose(string[] args, bool NeedCloseAfterPrint = true)
+        public static void CheckForHelpTag_PrintAndClose(string[] args, bool NeedCloseAfterPrint = true, string TagFilePath = null)
         {
             if (args.Length <= 0 || !IsItHelpTag(args[0]))
                 return;
 
             var lang = FindLanguageTagValue(args);
-            PrintSupportedLanguages();
-            PrintHelpInfo(lang); //en-Us, en, En - what lang or culture name you need
-            if (NeedCloseAfterPrint)
-                Environment.Exit(1);
+            PrintSupportedLanguages(TagFilePath);
+            PrintHelpInfo(lang, NeedCloseAfterPrint, TagFilePath); //en-Us, en, En - what lang or culture name you need
         }
 
         #endregion
 
         #region Tag Info
 
-        /// <summary>
-        /// Выводит информацию по тегу и останавливает работу программы
-        /// </summary>
-        /// <param name="tag">тег</param>
-        /// <param name="lang">язык</param>
-        public static void PrintTagInfoAndClose(string tag, string lang = null)
-        {
-            PrintTagInfo(tag, lang);
-            Environment.Exit(1);
-        }
         /// <summary> Вывод на консоль информации по тегу </summary>
         /// <param name="tag">искомый тег</param>
         /// <param name="lang">язык для вывода</param>
+        /// <param name="NeedCloseAfterPrint">Закрыть приложение если будет распечатан help</param>
         /// <param name="TagFilePath">путь к файлу тегов (если Null - используется PROJECT_NAME.Tags.json)</param>
         /// <returns></returns>
-        public static void PrintTagInfo(string tag, string lang = null, string TagFilePath = null)
+        public static void PrintTagInfo(string tag, string lang = null, bool NeedCloseAfterPrint = true, string TagFilePath = null)
         {
             var help_tags = GetLocalizedTags(lang, TagFilePath);
 
-            if (help_tags?.FindTag(tag) is not { } find)
-            {
-                UnknownTagMessage(tag).ConsoleRed();
-                return;
-            }
-
-            find.ConsolePrint();
+            PrintTag(help_tags, tag, NeedCloseAfterPrint);
         }
+
         /// <summary> Вывод на консоль информации по тегу </summary>
         /// <param name="tag">искомый тег</param>
         /// <param name="lang">язык для вывода</param>
+        /// <param name="NeedCloseAfterPrint">Закрыть приложение если будет распечатан help</param>
         /// <param name="TagFilePath">путь к файлу тегов (если Null - используется PROJECT_NAME.Tags.json)</param>
         /// <returns></returns>
-        public static async Task PringTagInfoAsync(string tag, string lang, string TagFilePath = null)
+        public static async Task PringTagInfoAsync(string tag, string lang, bool NeedCloseAfterPrint = true, string TagFilePath = null)
         {
             var help_tags = await GetLocalizedTagsAsync(lang, TagFilePath);
 
+            PrintTag(help_tags, tag, NeedCloseAfterPrint);
+        }
+
+        private static void PrintTag(LocalizedTags help_tags, string tag, bool NeedCloseAfterPrint)
+        {
             if (help_tags?.FindTag(tag) is not { } find)
             {
                 UnknownTagMessage(tag).ConsoleRed();
+                if (NeedCloseAfterPrint)
+                    Environment.Exit(1);
                 return;
             }
 
             find.ConsolePrint();
+
+            if (NeedCloseAfterPrint)
+                Environment.Exit(1);
         }
         private static string UnknownTagMessage(string tag) => $"UNKNOWN tag: {tag}\nUse -h or --help for more information";
 
@@ -219,7 +219,8 @@ namespace CLI.Tag.Helper
         /// <param name="args">аргументы</param>
         /// <param name="tag">тег</param>
         /// <param name="tag_index">индекс аргумента</param>
-        public static void CheckHelpArgAfterTag_PrintAndClose(string[] args, string tag, int tag_index)
+        /// <param name="TagFilePath">путь к файлу тегов (если Null - используется PROJECT_NAME.Tags.json)</param>
+        public static void CheckHelpArgAfterTag_PrintAndClose(string[] args, string tag, int tag_index, string TagFilePath = null)
         {
             var count = args.Length;
             if (tag_index < 0 || string.IsNullOrWhiteSpace(tag) || tag_index + 1 >= count)
@@ -231,13 +232,13 @@ namespace CLI.Tag.Helper
 
                 var lang = IsItLanguageTag(tag) ? null : FindLanguageTagValue(args);
 
-                var tag_help = new CliTags(lang).CurrentCultureTags?.Tags?.FirstOrDefault(t => t.Names.Contains(tag.Trim('-')));
-                tag_help?.ConsolePrint();
-                Environment.Exit(1);
+
+                var tag_help = new CliTags(lang, TagFilePath).CurrentCultureTags;
+                PrintTag(tag_help, tag, true);
             }
             else if (IsItLanguageTag(tag) && tag_index + 2 < count && IsItHelpTag(args[tag_index + 2]))
             {
-                PrintTagInfoAndClose(tag, arg);
+                PrintTagInfo(tag, arg, true, TagFilePath);
             }
         }
         /// <summary> Выводит информацию по тегу если за ним стоит тег -h или --help и закрывает приложение </summary>
@@ -248,19 +249,6 @@ namespace CLI.Tag.Helper
             var index = GetTagIndex(args, tag);
             CheckHelpArgAfterTag_PrintAndClose(args, tag, index);
         }
-
-        /// <summary> Выводит информацию по тегу если за ним стоит тег -h или --help и закрывает приложение </summary>
-        /// <param name="args">аргументы</param>
-        /// <param name="tag">тег</param>
-        /// <param name="tag_index">индекс аргумента</param>
-        public static void CheckHelpArgAfterTag_PrintAndClose(string[] args, char tag, int tag_index)
-            => CheckHelpArgAfterTag_PrintAndClose(args, $"{tag}", tag_index);
-
-        /// <summary> Выводит информацию по тегу если за ним стоит тег -h или --help и закрывает приложение </summary>
-        /// <param name="args">аргументы</param>
-        /// <param name="tag">тег</param>
-        public static void CheckHelpArgAfterTag_PrintAndClose(string[] args, char tag)
-            => CheckHelpArgAfterTag_PrintAndClose(args, $"{tag}");
 
         #endregion
 
@@ -292,14 +280,14 @@ namespace CLI.Tag.Helper
 
         #region Get
 
-        /// <summary> Получить список локализованных тегов </summary>
+        /// <summary> Получить локализованные теги </summary>
         /// <param name="lang">культура языка</param>
         /// <param name="TagFilePath">путь к файлу тегов (если Null - используется PROJECT_NAME.Tags.json)</param>
         /// <returns></returns>
         public static async Task<LocalizedTags> GetLocalizedTagsAsync(string lang, string TagFilePath = null) =>
             await Task.Run(() => GetLocalizedTags(lang, TagFilePath)).ConfigureAwait(false);
 
-        /// <summary> Получить список локализованных тегов </summary>
+        /// <summary> Получить локализованные теги </summary>
         /// <param name="lang">культура языка</param>
         /// <param name="TagFilePath">путь к файлу тегов (если Null - используется PROJECT_NAME.Tags.json)</param>
         /// <returns></returns>
@@ -308,13 +296,12 @@ namespace CLI.Tag.Helper
             var cli = new CliTags(lang, TagFilePath);
             return cli.CurrentCultureTags;
         }
-        /// <summary> Перечисление поддерживаемых локализаций </summary>
-        /// <param name="TagFilePath">путь к файлу тегов (если Null - используется PROJECT_NAME.Tags.json)</param>
 
         #endregion
 
         #region Поддержка языков
-
+        /// <summary> Перечисление поддерживаемых локализаций </summary>
+        /// <param name="TagFilePath">путь к файлу тегов (если Null - используется PROJECT_NAME.Tags.json)</param>
         public static async Task<IEnumerable<string>> GetSupportedLanguagesAsync(string TagFilePath = null) =>
             await Task.Run(() => GetSupportedLanguages(TagFilePath)).ConfigureAwait(false);
         /// <summary> Перечисление поддерживаемых локализаций </summary>
